7d7c41abbc6c61b790707fa7af6d00c4b87e3cba
mgalavai <myroslav.galavai@gmail.com>
2024-08-23 19:20:36 +0200

    Update3

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+// File: next.config.mjs
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+  }
+  
+  export default nextConfig;
